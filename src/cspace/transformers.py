979b98b6936b5f84dc4813ec1cf69fc3230afeec
Yong Tang <yong.tang.github@outlook.com>
2024-08-24 04:17:52 +0000

    Fix

--- src/cspace/transformers.py
+++ src/cspace/transformers.py
@@ -636,7 +636,7 @@ class PerceptionKinematics(cspace.torch.classes.PerceptionKinematics, JointState
         value = torch.concatenate(value, dim=-2)
 
         with torch.no_grad():
-            print("XXXX - ", observatoin.device)
+            print("XXXX - ", observation.device)
             print("YYYY - ", self.vision.device)
             pixel = self.vision(pixel_values=observation).last_hidden_state
 
